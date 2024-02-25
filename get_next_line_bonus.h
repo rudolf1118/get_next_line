a170d5636df706d4a9aa0f3e9ce54ff036a2cfea
Rudolf Harutyunyan <rud.haroutyunyan@gmail.com>
2024-02-25 15:40:52 +0400

    added bonus part, not perfectly tho

---
+++ get_next_line_bonus.h
@@ -0,0 +1,28 @@
+#ifndef GET_NEXT_LINE_BONUS
+# define GET_NEXT_LINE_BONUS
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+typedef struct s_list
+{
+    char *buf;
+    struct s_list *next                                                                                                          ;
+} t_list;
+
+void create_list (t_list **list, int fd);
+void append (t_list **list, char *buf);
+void polish_list(t_list **list);
+char *get_line(t_list *list);
+void deletealloc(t_list **list, t_list *clean_node, char *buf);
+int len_to_new_line (t_list *list);
+void copy_str(t_list *list, char *buf);
+t_list *find_last_node(t_list *list);
+int found_new_line(t_list *list);
+char *get_next_line(int fd);
+#endif // end of GET_NEXT_LINE_BONUS
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,106 @@
+#include "get_next_line_bonus.h"
+
+int found_new_line(t_list *list)
+{
+    int index;
+
+    if (!list)
+        return (0);
+    while (list)
+    {
+        index = 0;
+        while (list -> buf[index] && index < BUFFER_SIZE)
+        {
+            if (list -> buf[index] == '\n')
+                return (1);
+            ++index;
+        }
+        list = list -> next;
+    }
+    return (0);
+}
+
+void deletealloc(t_list **list, t_list *clean_node, char *buf)
+{
+    t_list *temp;
+
+    if (!(*list))
+        return ;
+    while (*list)
+    {
+        temp = (*list) ->next;
+        free((*list)->buf);
+        free(*list);
+        *list = temp;
+    }
+    *list = NULL;
+    if (clean_node->buf[0])
+        *list = clean_node;
+    else
+    {
+        free(buf);
+        free(clean_node);
+    }
+}
+
+
+t_list *find_last_node(t_list *list)
+{
+    if (!list)
+        return (NULL);
+    while (list -> next)
+        list = list->next;
+    return (list);
+}
+
+void	copy_str(t_list *list, char *str)
+{
+    int	i;
+    int	index;
+
+    index = 0;
+    if (!list)
+        return ;
+    while (list)
+    {
+        i = 0;
+        while (list->buf[i])
+        {
+            if (list->buf[i] == '\n')
+            {
+                str[index++] = '\n';
+                str[index] = '\0';
+                return ;
+            }
+            str[index++] = list->buf[i++];
+        }
+        list = list->next;
+    }
+    str[index] = '\0';
+}
+
+int	len_to_new_line(t_list *list)
+{
+    int	index;
+    int	len;
+
+    if (!list)
+        return (0);
+    len = 0;
+    while (list)
+    {
+        index = 0;
+        while (list->buf[index])
+        {
+            if (list->buf[index] == '\n')
+            {
+                len++;
+                return (len);
+            }
+            ++len;
+            ++index;
+        }
+        list = list->next;
+    }
+    return (len);
+}
